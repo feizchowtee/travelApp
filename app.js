4609c905273031888cd9e384d63b0afc9cc0f7b0
summitdesignmu <38588932+summitdesignmu@users.noreply.github.com>
2018-12-09 12:01:06 +0400

    first test

---
+++ app.js
@@ -0,0 +1,83 @@
+ScrollReveal().reveal('.device-left', {
+      reset: true,
+      origin: 'bottom',
+      distance: '100px',
+      duration: 1500,
+      scale: 0.5,
+      easing: 'cubic-bezier(.66,-0.17,.08,1.25)'
+});
+ScrollReveal().reveal('.device-right', {
+      reset: true,
+      origin: 'bottom',
+      distance: '100px',
+      duration: 1500,
+      easing: 'cubic-bezier(.66,-0.17,.08,1.25)',
+      scale: 0.5
+});
+ScrollReveal().reveal('.desc-right', {
+      reset: true,
+      origin: 'right',
+      distance: '100px',
+      duration: 2000,
+      easing: 'cubic-bezier(.66,-0.17,.08,1.25)',
+});
+ScrollReveal().reveal('.desc-left', {
+      reset: true,
+      origin: 'left',
+      easing: 'cubic-bezier(.66,-0.17,.08,1.25)',
+      distance: '100px',
+      duration: 2000
+});
+ScrollReveal().reveal('.hero-img', {
+      reset: true,
+      scale:.95,
+      origin: 'top',
+      //distance: '100px',
+      easing: 'cubic-bezier(.66,-0.17,.08,1.25)',
+      duration: 2000
+});
+ScrollReveal().reveal('.hero-h1', {
+      reset: true,
+      distance: '10px',
+      origin: 'top',
+      duration: 2000,
+      easing: 'cubic-bezier(.66,-0.17,.08,1.25)'
+});
+ScrollReveal().reveal('.hero-p', {
+      reset: true,
+      distance: '10px',
+      origin: 'bottom',
+      duration: 2000,
+      easing: 'cubic-bezier(.66,-0.17,.08,1.25)'
+});
+ScrollReveal().reveal('p', {
+      reset: true,
+      distance: '10px',
+      origin: 'bottom',
+      duration: 2000,
+      easing: 'cubic-bezier(.66,-0.17,.08,1.25)',
+      
+});
+ScrollReveal().reveal('h4', {
+      reset: true,
+      scale:0.8,
+      duration: 2000,
+      easing: 'cubic-bezier(.66,-0.17,.08,1.25)',
+
+});
+ScrollReveal().reveal('.benefit-img', {
+      reset: true,
+      distance: '10px',
+      origin: 'top',
+      duration: 2000,
+      easing: 'cubic-bezier(.66,-0.17,.08,1.25)',
+
+});
+ScrollReveal().reveal('h2', {
+      reset: true,
+      distance: '50px',
+      origin: 'left',
+      duration: 2000,
+      easing: 'cubic-bezier(.66,-0.17,.08,1.25)',
+
+});
